640e983dfc1a309f64ad5a0f7e45c7e6b68f7b0c
Michał Roszczenko <michal.roszczenko@poczta.onet.pl>
2023-07-13 12:21:54 +0200

    Fixed ProductionTimeCalculation method

--- Production.Domain/Entities/Production.cs
+++ Production.Domain/Entities/Production.cs
@@ -5,12 +5,12 @@
         public int Id { get; set; }
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
-        public int ProductionTimeInHours { get; set; }
+        public int ProductionTimeInHours { get; private set; }
         public InjectionMold InjectionMold { get; set; } = default!;
         public Guid InjectionMoldId { get; set; }
         public InjectionMoldingMachine InjectionMoldingMachine { get; set; } = default!;
         public int InjectionMoldingMachineId { get; set; }
 
-        public void ProductionTimeCalculation() => ProductionTimeInHours = (End - Start).Hours;
+        public void ProductionTimeCalculation() => ProductionTimeInHours = (int)Math.Ceiling((End - Start).TotalMinutes/60);
     }
 }
