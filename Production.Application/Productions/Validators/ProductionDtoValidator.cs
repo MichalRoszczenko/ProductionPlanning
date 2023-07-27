1e899bf3492ea5fb7c35b319b8a80f1767f46e24
Michał Roszczenko <michal.roszczenko@poczta.onet.pl>
2023-07-27 22:18:19 +0200

    Added  Validation for block multi productions with the same mold at the same time

--- Production.Application/Productions/Validators/ProductionDtoValidator.cs
+++ Production.Application/Productions/Validators/ProductionDtoValidator.cs
@@ -5,8 +5,12 @@ namespace Production.Application.Productions.Validators
 {
     public class ProductionDtoValidator : AbstractValidator<ProductionDto>
     {
+        private readonly IInjectionMoldRepository _moldRepository;
+
         public ProductionDtoValidator(IInjectionMoldRepository moldRepository, IInjectionMoldingMachineRepository machineRepository)
         {
+            _moldRepository = moldRepository;
+
             RuleFor(e => e.Start)
                 .NotNull()
                 .NotEmpty();
@@ -20,6 +24,7 @@ namespace Production.Application.Productions.Validators
             RuleFor(e => e.InjectionMoldId)
                 .NotEmpty()
                 .NotNull()
+                .Must(InjectionMoldIsAvailable).WithMessage("The injection mold is scheduled for another production at this time")
                 .Custom((value, context) =>
                 {
                     var existingMold = moldRepository.GetById(value).Result;
@@ -42,6 +47,20 @@ namespace Production.Application.Productions.Validators
                         context.AddFailure($"Machine ID: \"{value}\" doesn't exist.");
                     }
                 });
+        }
+
+        private bool InjectionMoldIsAvailable(ProductionDto productionDto, Guid moldId)
+        {
+            var mold = _moldRepository.GetById(moldId, true).Result;
+
+            var moldProductions = mold!.Productions!.Where(p => p.Id != productionDto.Id);
+
+            var overlapedProduction =  moldProductions.Any(p =>
+                (productionDto.Start >= p.Start && productionDto.Start <= p.End) ||
+                (productionDto.End >= p.Start && productionDto.End <= p.End) ||
+                (productionDto.Start <= p.Start && productionDto.End >= p.End));
+
+            return !overlapedProduction;
         }
     }
 }
