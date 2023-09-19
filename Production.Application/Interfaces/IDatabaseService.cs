de0fbc6a9192796474e68efa50bcd4339b6cacc2
Michał Roszczenko <michal.roszczenko@poczta.onet.pl>
2023-09-19 16:51:24 +0200

    Created IDatabaseService

---
+++ Production.Application/Interfaces/IDatabaseService.cs
@@ -0,0 +1,13 @@
+﻿namespace Production.Application.Interfaces
+{
+    public interface IDatabaseService<T, Y>
+        where T : class
+        where Y : struct
+    {
+        Task Create(T itemDto);
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(Y itemId, bool withProductionInfo = false);
+        Task Remove(Y itemId);
+        Task Update(Y itemId, T itemDto);
+    }
+}
