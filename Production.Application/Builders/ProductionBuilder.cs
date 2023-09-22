fdde30b60fec5bff45f6def16ac79f29c1cfd1c1
Michał Roszczenko <michal.roszczenko@poczta.onet.pl>
2023-09-22 12:52:44 +0200

    Created ProductionBuilder

---
+++ Production.Application/Builders/ProductionBuilder.cs
@@ -0,0 +1,50 @@
+﻿using Production.Application.Dtos;
+using Production.Domain.Entities;
+
+namespace Production.Application.Builders
+{
+	internal sealed class ProductionBuilder
+	{
+		private Domain.Entities.Production _production { get; set; }
+
+		public ProductionBuilder Init(Domain.Entities.Production production)
+		{
+			_production = production;
+			return this;
+		}
+
+		public ProductionBuilder CalculateProductionTime()
+		{
+			_production.ProductionTimeCalculation();
+			return this;
+		}
+
+		public ProductionBuilder AddMaterialStatus(InjectionMold injectionMold, Material material)
+		{
+			var materialRequirements = CreateMaterialRequirements(injectionMold);
+
+			material.Stock.PlannedMaterialDemand += materialRequirements.Usage;
+
+			material.Stock.CountMaterialToOrder();
+
+			_production.MaterialStatus.MaterialIsAvailable = material.Stock.PlannedMaterialDemand <= material.Stock.MaterialInStock;
+			_production.MaterialStatus.MaterialUsage = materialRequirements.Usage;
+
+			return this;
+		}
+
+		private MaterialRequirements CreateMaterialRequirements(InjectionMold injectionMold)
+		{
+			var consumption = injectionMold!.Consumption;
+
+			var productionTime = _production.ProductionTimeInHours;
+
+			return new MaterialRequirements(productionTime, consumption);
+		}
+
+		public Domain.Entities.Production Build()
+		{
+			return _production;
+		}
+	}
+}
