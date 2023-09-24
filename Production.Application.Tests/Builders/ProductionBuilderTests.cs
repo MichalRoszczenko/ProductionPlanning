0719a4bd89938ebd58ac279294e8f07e1c09471b
Michał Roszczenko <michal.roszczenko@poczta.onet.pl>
2023-09-24 16:36:32 +0200

    Created ProductionBuilder Init test

---
+++ Production.Application.Tests/Builders/ProductionBuilderTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Production.Application.Builders;
+using Xunit;
+
+namespace Production.Application.Tests.Builders
+{
+	public class ProductionBuilderTests
+	{
+		private readonly ProductionBuilder _productionBuilder;
+
+		public ProductionBuilderTests()
+        {
+            _productionBuilder = new ProductionBuilder();
+        }
+
+        [Fact()]
+		public void Init_ReturnProduction_ForExistProduction()
+        {
+			//arrange
+			var production = new Domain.Entities.Production()
+			{
+				Start = DateTime.Now,
+				End = DateTime.Now.AddHours(25),
+				InjectionMoldId = Guid.NewGuid(),
+				InjectionMoldingMachineId = 5
+			};
+
+			//act
+			_productionBuilder.Init(production);
+            var result = _productionBuilder.Build();
+
+			//assert
+            result.Should().Be(production);
+        }
+	}
+}
